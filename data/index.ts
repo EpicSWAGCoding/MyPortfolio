66d6464cc0025dada066247ea77bb2467b5f5303
Egor <EpicSWAG123@yandex.ru>
2024-06-20 22:45:28 +0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,214 @@
+export const navItems = [
+    { name: "Обо мне", link: "#about" },
+    { name: "Проекты", link: "#projects" },
+    { name: "Отзывы", link: "#testimonials" },
+    { name: "Связаться", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "Мои клиенты всегда на первом месте, обеспечивая прозрачное и эффективное взаимодействие",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "Работаю в удобное для вас время, независимо от часовых поясов",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "Cтек технологий",
+        description: "Всегда стремлюсь к совершенству",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Технический эксперт с глубоким пониманием и страстью к инновациям",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Сейчас работаю над инновационной библиотекой JS для анимаций",
+        description: "Внутренняя информация",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Готовы к сотрудничеству? Давайте создадим нечто удивительное вместе!",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "Anons",
+        des: "Управления и организации мероприятий внутри ВУЗА.",
+        img: "/p1.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/c.svg", "/next.svg"],
+        link: "https://anons-events.vercel.app/",
+    },
+    // {
+    //     id: 2,
+    //     title: "Yoom - Video Conferencing App",
+    //     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    //     img: "/p2.svg",
+    //     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    //     link: "/ui.yoom.com",
+    // },
+    // {
+    //     id: 3,
+    //     title: "AI Image SaaS - Canva Application",
+    //     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    //     img: "/p3.svg",
+    //     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    //     link: "/ui.aiimg.com",
+    // },
+    // {
+    //     id: 4,
+    //     title: "Animated Apple Iphone 3D Website",
+    //     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    //     img: "/p4.svg",
+    //     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //     link: "/ui.apple.com",
+    // },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Работать с Егором было настоящим удовольствием. Его глубокие знания в области фронтенд-разработки, внимательность к деталям и способность быстро адаптироваться к изменениям сделали наш проект успешным. Рекомендую Егора как надежного и креативного специалиста.",
+        name: "Анна Смирнова",
+        title: "Главный менеджер проекта, «ТехИнновации»"
+    },
+    {
+        quote: "Егор быстро стал незаменимым членом нашей команды благодаря своим выдающимся навыкам фронтенд-разработки. Его креативный подход и способность находить оптимальные решения позволили нам значительно улучшить наш веб-продукт. Я бы с удовольствием снова работал с Егором.",
+        name: "Мария Кузнецова",
+        title: "Руководитель отдела разработки, «ИнтерСофт»"
+    },
+    {
+        quote: "Егор продемонстрировал исключительные навыки и профессионализм в ходе нашего сотрудничества. Его внимание к деталям и способность решать сложные задачи были крайне полезны для нашего проекта. Очень рекомендую его как эксперта в области фронтенд-разработки.",
+        name: "Иван Петров",
+        title: "Технический директор, «СофтСистемс»"
+    },
+    {
+        quote: "Сотрудничество с Егором было исключительно положительным опытом. Его стремление к совершенству и готовность идти на дополнительные усилия ради успеха проекта заслуживают особого упоминания. Егор — настоящий профессионал, и я рад, что нам удалось с ним поработать.",
+        name: "Александр Иванов",
+        title: "Директор по развитию, «ВебТех»"
+    },
+    {
+        quote: "Егор продемонстрировал высокий уровень профессионализма и знаний в ходе нашего проекта. Его умение решать сложные задачи и предлагать инновационные решения сыграли ключевую роль в успехе нашей команды. Очень рекомендую его как квалифицированного фронтенд-разработчика.",
+        name: "Елена Васильева",
+        title: "Менеджер по продукту, «ТехноВеб»"
+    },
+];
+
+// export const companies = [
+//     {
+//         id: 1,
+//         name: "cloudinary",
+//         img: "/cloud.svg",
+//         nameImg: "/cloudName.svg",
+//     },
+//     {
+//         id: 2,
+//         name: "appwrite",
+//         img: "/app.svg",
+//         nameImg: "/appName.svg",
+//     },
+//     {
+//         id: 3,
+//         name: "HOSTINGER",
+//         img: "/host.svg",
+//         nameImg: "/hostName.svg",
+//     },
+//     {
+//         id: 4,
+//         name: "stream",
+//         img: "/s.svg",
+//         nameImg: "/streamName.svg",
+//     },
+//     {
+//         id: 5,
+//         name: "docker.",
+//         img: "/dock.svg",
+//         nameImg: "/dockerName.svg",
+//     },
+// ];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "фронтенд-разработчик стажер",
+        desc: "Помогал в разработке веб-платформы с использованием React.js, повышающей интерактивность.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Разработка мобильных приложений — JSM Tech",
+        desc: "Спроектировал и разработал мобильное приложение для платформ iOS и Android с использованием React Native.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "Freelance",
+        desc: "Руководил разработкой мобильного приложения для клиента от первоначальной концепции до развертывания в магазинах приложений.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp3.svg",
+    },
+    {
+        id: 4,
+        title: "Ведущий фронтенд-разработчик",
+        desc: "Разрабатывал и поддерживал функции взаимодействия с пользователем с использованием современных интерфейсных технологий.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/EpicSWAGCoding"
+    },
+    // {
+    //     id: 2,
+    //     img: "/twit.svg",
+    // },
+    // {
+    //     id: 3,
+    //     img: "/link.svg",
+    // },
+];
