53819595576a1bc42ce5b3f580eea0e07718d047
Egor <EpicSWAG123@yandex.ru>
2024-06-20 22:49:31 +0300

    first commit 2.0

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from "react";
-
-import { companies, testimonials } from "@/data";
+// import { companies, testimonials } from "@/data";
+import { testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
